f67292f6716d485318a53ddd963d1bd2f2dcc6f8
Johnny-Wins <98790284+Johnny-Wins@users.noreply.github.com>
2023-04-16 17:19:18 -0400

    Added MageStuff folder
    added a folder for a new project where I'll be building a character builder for mage: the awakening

---
+++ MageStuff/characterMaker.py
@@ -0,0 +1,35 @@
+'''This program can create characters for the game Mage: The Awakening.
+
+It will include a description of this process, and then a system to carry it out.
+
+Step 1: Character Concept
+    Concept
+
+        • Activist - Journalist, blogger, lobbyist, malcontent.
+        • Artist - Writer, dancer, painter, musician.
+        • Athlete - Team player, personal trainer, physical artisan, 
+        thrill-seeker.
+        • Caretaker - Parent, teacher, social worker, medical 
+        professional.
+        • Criminal - Gang member, con artist, drug dealer, thief.
+        • Executive - Tycoon, manager, facilitator, power-player. 
+        • Guardian - Cop, security guard, conspiracy theorist, 
+        social watchdog. 
+        • Intellectual - Scholar, professor, philosopher, social critic.
+        • Kid - Child, student, innocent, gutter-punk. 
+        • Laborer - Factory worker, sales clerk, roadie, construction 
+        worker.
+        • Mystic - Priest/ ess, hermit, shaman, enlightened weirdo.
+        • Night-Owl - Bartender, bouncer, club-goer, celebrity.
+        • Rebel - Vagabond, outlaw, urban tribalist, subculture 
+        devotee. 
+        • Technician - Mechanic, lab tech, craftsman, computer 
+        geek.
+
+    Affiliation
+    
+    Essence
+    Archetypes
+
+Step 2: Select Attributes
+'''
